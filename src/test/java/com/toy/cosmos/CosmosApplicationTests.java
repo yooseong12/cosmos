2f6b04749e0dd048537c65a8fdb525929e7f5133
yooseong12 <rladbtjd81@gmail.com>
2022-12-14 15:55:44 +0900

    init

---
+++ src/test/java/com/toy/cosmos/CosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.cosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
